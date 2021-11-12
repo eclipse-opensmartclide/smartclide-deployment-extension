778a1bdbb167bbe32a89b74b7c02f1941459c1e6
Adrián Díaz <adiaz@wtelecom.es>
2021-11-12 12:43:30 +0100

    Add improve widget but disable

--- smartclide-deployment-widget/src/browser/smartclide-deployment-widget-frontend-module.ts
+++ smartclide-deployment-widget/src/browser/smartclide-deployment-widget-frontend-module.ts
@@ -1,16 +1,27 @@
-import { ContainerModule } from '@theia/core/shared/inversify';
-import { SmartclideDeploymentWidgetWidget } from './smartclide-deployment-widget-widget';
-import { SmartclideDeploymentWidgetContribution } from './smartclide-deployment-widget-contribution';
-import { bindViewContribution, FrontendApplicationContribution, WidgetFactory } from '@theia/core/lib/browser';
+import { ContainerModule } from "@theia/core/shared/inversify";
+import { SmartclideDeploymentWidgetWidget } from "./smartclide-deployment-widget-widget";
+import { SmartclideDeploymentWidgetContribution } from "./smartclide-deployment-widget-contribution";
+import {
+  bindViewContribution,
+  FrontendApplicationContribution,
+  WidgetFactory,
+} from "@theia/core/lib/browser";
 
-import '../../src/browser/style/index.css';
+import "../../src/browser/style/index.css";
 
-export default new ContainerModule(bind => {
-    bindViewContribution(bind, SmartclideDeploymentWidgetContribution);
-    bind(FrontendApplicationContribution).toService(SmartclideDeploymentWidgetContribution);
-    bind(SmartclideDeploymentWidgetWidget).toSelf();
-    bind(WidgetFactory).toDynamicValue(ctx => ({
-        id: SmartclideDeploymentWidgetWidget.ID,
-        createWidget: () => ctx.container.get<SmartclideDeploymentWidgetWidget>(SmartclideDeploymentWidgetWidget)
-    })).inSingletonScope();
+export default new ContainerModule((bind) => {
+  bindViewContribution(bind, SmartclideDeploymentWidgetContribution);
+  bind(FrontendApplicationContribution).toService(
+    SmartclideDeploymentWidgetContribution
+  );
+  bind(SmartclideDeploymentWidgetWidget).toSelf();
+  bind(WidgetFactory)
+    .toDynamicValue((ctx) => ({
+      id: SmartclideDeploymentWidgetWidget.ID,
+      createWidget: () =>
+        ctx.container.get<SmartclideDeploymentWidgetWidget>(
+          SmartclideDeploymentWidgetWidget
+        ),
+    }))
+    .inSingletonScope();
 });
--- smartclide-deployment-widget/src/browser/smartclide-deployment-widget-widget.tsx
+++ smartclide-deployment-widget/src/browser/smartclide-deployment-widget-widget.tsx
@@ -1,39 +1,97 @@
-import * as React from 'react';
-import { injectable, postConstruct, inject } from '@theia/core/shared/inversify';
-import { AlertMessage } from '@theia/core/lib/browser/widgets/alert-message';
-import { ReactWidget } from '@theia/core/lib/browser/widgets/react-widget';
-import { MessageService } from '@theia/core';
+import * as React from "react";
+import {
+  injectable,
+  postConstruct,
+  inject,
+} from "@theia/core/shared/inversify";
+import { ReactWidget } from "@theia/core/lib/browser/widgets/react-widget";
+import { MessageService } from "@theia/core";
+import { LocalStorageService } from "@theia/core/lib/browser/";
 
 @injectable()
 export class SmartclideDeploymentWidgetWidget extends ReactWidget {
+  static readonly ID = "smartclide-deployment-widget:widget";
+  static readonly LABEL = "SmartclideDeploymentWidget Widget";
+  static state = {
+    username: "",
+    apiToken: "",
+  };
 
-    static readonly ID = 'smartclide-deployment-widget:widget';
-    static readonly LABEL = 'SmartclideDeploymentWidget Widget';
+  @inject(MessageService)
+  protected readonly messageService!: MessageService;
+  @inject(LocalStorageService)
+  private readonly localStorageService: LocalStorageService;
+  @postConstruct()
+  protected async init(): Promise<void> {
+    this.id = SmartclideDeploymentWidgetWidget.ID;
+    this.title.label = SmartclideDeploymentWidgetWidget.LABEL;
+    this.title.caption = SmartclideDeploymentWidgetWidget.LABEL;
+    this.title.closable = true;
+    this.title.iconClass = "fa fa-window-maximize"; // example widget icon.
+    this.update();
+  }
 
-    @inject(MessageService)
-    protected readonly messageService!: MessageService;
-
-    @postConstruct()
-    protected async init(): Promise < void> {
-        this.id = SmartclideDeploymentWidgetWidget.ID;
-        this.title.label = SmartclideDeploymentWidgetWidget.LABEL;
-        this.title.caption = SmartclideDeploymentWidgetWidget.LABEL;
-        this.title.closable = true;
-        this.title.iconClass = 'fa fa-window-maximize'; // example widget icon.
-        this.update();
-    }
-
-    render(): React.ReactElement {
-        const header = `This is a sample widget which simply calls the messageService
-        in order to display an info message to end users.`;
-        return <div id='widget-container'>
-            <AlertMessage type='INFO' header={header} />
-            <button className='theia-button secondary' title='Display Message' onClick={_a => this.displayMessage()}>Display Message</button>
+  render(): React.ReactElement {
+    return (
+      <>
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col-xs-12 col-md-6 col-md-offset-3">
+              <div className="well">
+                <div className="row">
+                  <div className="form-group col-xs-12">
+                    <label htmlFor="username" className="text-black ">
+                      Gitlab Username
+                    </label>
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="username"
+                      placeholder="Gitlab Username"
+                      name="username"
+                      onChange={this.updateInput}
+                    />
+                  </div>
+                  <div className="form-group col-xs-12">
+                    <label htmlFor="apitoken" className="text-black ">
+                      Your Api Token
+                    </label>
+                    <input
+                      type="email"
+                      className="form-control"
+                      id="apitoken"
+                      placeholder="Your Api Token"
+                      name="apitoken"
+                      onChange={this.updateInput}
+                    />
+                  </div>
+                  <div className="form-group col-xs-12 text-rigth">
+                    <button
+                      className="btn btn-primary "
+                      title="Display Message"
+                      onClick={(_a) =>
+                        this.localStorageService.setData(
+                          "config",
+                          JSON.stringify({
+                            ...SmartclideDeploymentWidgetWidget.state,
+                          })
+                        )
+                      }
+                    >
+                      Save
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-    }
-
-    protected displayMessage(): void {
-        this.messageService.info('Congratulations: SmartclideDeploymentWidget Widget Successfully Created!');
-    }
-
+      </>
+    );
+  }
+  protected updateInput(e: React.ChangeEvent<HTMLInputElement>) {
+    const key = e.currentTarget
+      .name as keyof typeof SmartclideDeploymentWidgetWidget.state;
+    SmartclideDeploymentWidgetWidget.state[key] = e.currentTarget.value;
+  }
 }
