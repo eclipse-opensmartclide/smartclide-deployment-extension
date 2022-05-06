b82bd9a35a2fbc8dbebd2ee518876a73081c27f1
1ld <onelifedesigning@gmail.com>
2022-05-06 09:32:36 +0200

    Add Monitoring and table

--- deployment-widget/src/browser/app/domain/Deployment.tsx
+++ deployment-widget/src/browser/app/domain/Deployment.tsx
@@ -1,20 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+
+import Spinner from '../componets/Spinner';
+import Button from '../componets/Button';
 
 interface DeploymentProps {}
 
 const Deployment: React.FC<DeploymentProps> = () => {
-  return (
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const timmer = setTimeout(() => {
+      setLoading(false);
+    }, 5000);
+    return () => {
+      clearTimeout(timmer);
+    };
+  }, []);
+
+  return !loading ? (
     <div>
       <h1>Deployments</h1>
       <table>
-        <th>
-          <td>Id</td>
-        </th>
-        <tr>
-          <td>1</td>
-        </tr>
+        <thead>
+          <tr>
+            <th>Id</th>
+            <th>Date</th>
+            <th>Status</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>{new Date()}</td>
+            <td>stoped</td>
+            <td>
+              <Button className="btn-danger">Delete</Button>
+            </td>
+          </tr>
+        </tbody>
       </table>
     </div>
+  ) : (
+    <Spinner isVisible={loading} />
   );
 };
 
