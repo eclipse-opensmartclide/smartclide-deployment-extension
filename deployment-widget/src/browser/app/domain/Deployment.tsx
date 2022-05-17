62d83f488bd0b802b2cfe05fa8be5b2fbb57b486
Adrián Díaz <adiaz@wtelecom.es>
2022-05-17 13:28:04 +0200

    Add logic read settings file and get list from api

--- deployment-widget/src/browser/app/domain/Deployment.tsx
+++ deployment-widget/src/browser/app/domain/Deployment.tsx
@@ -1,45 +1,69 @@
 import React, { useState, useEffect } from 'react';
 
+import { useBackendContext } from '../contexts/BackendContext';
+import { getDeploymentList } from '../../../common/fetchMethods';
+
 import Spinner from '../componets/Spinner';
-import Button from '../componets/Button';
+import TableWidhtAction from '../componets/Table/TableWidhtAction';
 
 interface DeploymentProps {}
+interface Settings {
+  k8sUrl: string;
+  k8sToken: string;
+  project: string;
+  gitLabToken: string;
+  branch: string;
+  replicas: string;
+}
+interface Pagination {
+  skip: string;
+  limit: string;
+}
+
+const initialPagination: Pagination = {
+  skip: '0',
+  limit: '25',
+};
 
 const Deployment: React.FC<DeploymentProps> = () => {
   const [loading, setLoading] = useState<boolean>(true);
+  const [pagination] = useState<Pagination>(initialPagination);
+  const { backend } = useBackendContext();
+  const { workspaceService, backendService } = backend;
 
   useEffect(() => {
-    const timmer = setTimeout(() => {
-      setLoading(false);
-    }, 5000);
-    return () => {
-      clearTimeout(timmer);
-    };
+    (async () => {
+      const currentPath =
+        workspaceService.workspace?.resource.path.toString() || '';
+      const prevSettings: Settings =
+        currentPath &&
+        JSON.parse(
+          await backendService.fileRead(
+            `${currentPath}/.smartclide-settings.json`
+          )
+        );
+      //TODO: If dont have settnigs show conosole msg and return
+      const { gitLabToken, project } = prevSettings;
+      const builds =
+        gitLabToken &&
+        project &&
+        (await getDeploymentList(
+          project,
+          gitLabToken,
+          pagination.limit,
+          pagination.skip
+        ));
+      console.log('builds', builds);
+      builds && setLoading(false);
+    })();
   }, []);
+
+  const handleGetStatus = (id: string) => {};
 
   return !loading ? (
     <div>
       <h1>Deployments</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Id</th>
-            <th>Date</th>
-            <th>Status</th>
-            <th>Actions</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>{new Date()}</td>
-            <td>stoped</td>
-            <td>
-              <Button className="btn-danger">Delete</Button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+      <TableWidhtAction columns={[]} dataSource={[]} action={handleGetStatus} />
     </div>
   ) : (
     <Spinner isVisible={loading} />
