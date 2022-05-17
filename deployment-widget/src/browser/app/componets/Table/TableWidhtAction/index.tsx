b3d6b6c590bb647d36a96af436202465043cfef4
Adrián Díaz <adiaz@wtelecom.es>
2022-05-17 13:27:28 +0200

    Add TableWidthActions Component

---
+++ deployment-widget/src/browser/app/componets/Table/TableWidhtAction/index.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import Button from '../../Button';
+
+interface TableProps {
+  columns: [];
+  dataSource: SourceData[];
+  action: (i: string) => void;
+}
+interface SourceData {
+  id: string;
+  created?: string;
+  state?: string;
+  username?: string;
+  repository?: string;
+  branch?: string;
+  updated?: string;
+}
+
+const TableWidthAction: React.FC<TableProps> = (props) => {
+  const { columns, dataSource, action } = props;
+  return (
+    <table>
+      <thead>
+        {columns.length !== 0 && (
+          <tr>
+            {columns.map((col) => (
+              <th>{col}</th>
+            ))}
+          </tr>
+        )}
+      </thead>
+      <tbody>
+        {dataSource.length !== 0 ? (
+          dataSource.map((data, index) => {
+            return (
+              <tr key={index}>
+                <td>{data.id}</td>
+                <td>{data.created}</td>
+                <td>{data.updated}</td>
+                <td>{data.state}</td>
+                <td>{data.repository}</td>
+                <td>{data.username}</td>
+                <td>{data.branch}</td>
+                <td>
+                  <Button
+                    className="btn-primary"
+                    onClick={() => action(data.id)}
+                  >
+                    Deploy
+                  </Button>
+                </td>
+              </tr>
+            );
+          })
+        ) : (
+          <tr>
+            <td colSpan={columns.length !== 0 ? columns.length : 1}>No data</td>
+          </tr>
+        )}
+      </tbody>
+    </table>
+  );
+};
+
+export default TableWidthAction;
